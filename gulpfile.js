d7b6925168c2db02474b24c6ca148c619c56aff6
Kyle Logue <klogue214@gmail.com>
2018-03-30 12:46:12 -0400

    Initial structure and ideas

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: './'
+    });
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+    gulp.watch('docs/**/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+    return gulp.src('src/scss/mustard-ui.scss')
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions', '> 2%'],
+        cascade: false
+    }))
+    .pipe(rename({
+        suffix:'.min'
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve', 'sass']);
